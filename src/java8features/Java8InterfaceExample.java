fc1f5dfecd068215a838bc5d38c43f07c0326915
Alexander Hartson <alexander-hartson@live.com>
2018-05-07 23:58:27 -0500

    adding files

--- src/java8features/Java8InterfaceExample.java
+++ src/java8features/Java8InterfaceExample.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Java8InterfaceExample {
     public static void main(String[] args) {
         Domesticated myDog = new Dog();
-        Domesticated.registor(myDog);
+        Domesticated.register(myDog);
         myDog.doWork();
         myDog.doWork("play fetch!");
     }
@@ -14,4 +14,32 @@ public class Java8InterfaceExample {
 interface Domesticated {
     ArrayList<String> animals = new ArrayList<>();
     ArrayList<String> plants = new ArrayList<>();
+
+    static void register(Domesticated species) {
+        if (species instanceof  Animal)
+            animals.add(species.getClass().getName());
+        else if (species instanceof Plant)
+            plants.add(species.getClass().getName());
+    }
+
+    void doWork();
+
+    default void doWork(String job) {
+        System.out.println("I " + job);
+    }
+}
+
+abstract class Animal {}
+abstract class Plant {}
+
+class Dog extends Animal implements Domesticated {
+    public void doWork() {
+        System.out.println("I hunt for food!");
+    }
+}
+
+class Wheat extends Plant implements Domesticated {
+    public void doWork() {
+        System.out.println("I produce large quantities of grain");
+    }
 }
